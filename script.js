1373d9a91f5c903cd8a072654db1c368ffd470ae
gabriellicabral <gabrielli.cabral2017@gmail.com>
2023-12-05 23:04:49 -0300

    Primeira atualização

---
+++ script.js
@@ -0,0 +1,26 @@
+let cantora = `Mariah Carey`
+let ano = 2023
+let idade = 54
+console.log(ano=ano-idade)
+console.log(`A data de nascimento da ${cantora} é ${ano}`)
+
+let quartaFeira = false
+if (quartaFeira)  {
+    console.log(`Hoje é quarta-feira`)
+} else {`Hoje não é quarta-feira`}
+console.log(typeof quartaFeira)
+
+let _idade = 16
+if(_idade >= 18) {
+    console.log(`Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey`)} 
+else{
+    console.log(`Sinto muito, mas assistirá a tv globo`)}
+
+    let mês1 = `Agosto`
+    let mês2 = `Dezembro`
+    let mês3 = `Junho`
+    if (mês1 === `Agosto` || mês2 === `Dezembro`|| mês3 === `Junho` ) {
+        console.log (`Uma ou mais opções estão corretas. O mês escolhido foi o mês de Dezembro`) } 
+       else {
+        console.log(`Algo de errado não está certo, o mês digitado foi Dezembro`)
+       } 
